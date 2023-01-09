e13c7147796ce5375c8d4f0c79ead55c246e119b
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-09 15:34:07 +0000

    add function doc

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,5 +1,16 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_depth - Finds how deep from the root node
+ * a tree node 'tree' is.
+ *
+ * If 'tree' is NULL or if 'tree' has no parent,
+ * you just get 0.
+ *
+ * @tree: node to find its depth in a binary tree
+ *
+ * Return: depth in tree
+ */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (tree && tree->parent)
