aa282c37e0911a74e0d9f159143fa2f186e0f4e3
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-09 16:12:07 +0000

    add counting all non-leaf nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Counts all nodes in tree
+ * 'tree', treating the 'tree' node as the root,
+ * that have AT LEAST ONE CHILD.
+ *
+ * If tree is NULL or if tree has no children,
+ * result is 0.
+ *
+ * (SO BASICALLY, THE OPPOSITE OF 'binary_tree_leaves')
+ *
+ * @tree: root node of tree to count nodes in
+ *
+ * Return: Count of all nodes with at least one
+ * child in tree 'tree', with the 'tree' node
+ * as root
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		if (tree->left || tree->right)
+		{
+			/* self */
+			size_t result = 1;
+
+			/* children */
+			result += binary_tree_nodes(tree->left);
+			result += binary_tree_nodes(tree->right);
+
+			return (result);
+		}
+	}
+
+	return (0);
+}
