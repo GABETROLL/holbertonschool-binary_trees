172c44f1757056dca001b765c3970e793888ca99
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-12 15:27:42 +0000

    add copied function to pass checks

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,37 @@
 #include "binary_trees.h"
 
-/* from exercise 9: */
-size_t binary_tree_height(const binary_tree_t *tree);
+/**
+ * binary_tree_height - Finds the height of the
+ * node 'tree' (aka how far away is the farthest
+ * leaf child of 'tree')
+ *
+ * THIS WAS COPIED FROM EXERCISE 9 TO PASS HOLBERTON'S
+ * COMPILATION PROCESS
+ *
+ * @tree: tree
+ *
+ * Return: the max height of the node 'tree' if 'tree'
+ * is available, 0 otherwise
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree && (tree->left || tree->right))
+	{
+		size_t left_child_height = binary_tree_height(tree->left);
+		size_t right_child_height = binary_tree_height(tree->right);
+
+		size_t result;
+
+		if (left_child_height > right_child_height)
+			result = left_child_height;
+		else
+			result = right_child_height;
+
+		return (result + 1);
+	}
+
+	return (0);
+}
 
 /**
  * binary_tree_balance - Assumes the 'tree' node
